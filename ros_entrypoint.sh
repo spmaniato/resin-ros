742153b6ea0383a228b5e1de2795c09e789e0e28
Spyros Maniatopoulos <spmaniato@gmail.com>
2016-08-15 20:38:55 -0500

    [entrypoint] Single machine configuration

--- ros_entrypoint.sh
+++ ros_entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -e
 
-# Setup ROS environment
+# Set up ROS environment
 source "$ROS_INSTALL_DIR/setup.bash"
-# source "$CATKIN_WS/install/setup.bash"
+
+# Single machine configuration (until a better solution is found)
+export ROS_HOSTNAME=localhost
+export ROS_MASTER_URI=http://localhost:11311
+
 exec "$@"
